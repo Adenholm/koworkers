d4079472b44ac70c67463250f5985eb2b0c3a486
Zeeta00 <69895254+Zeeta00@users.noreply.github.com>
2021-09-17 17:01:00 +0200

    added hashmap to board, made basic move

--- app/src/main/java/com/example/koworkers/model/Board.java
+++ app/src/main/java/com/example/koworkers/model/Board.java
@@ -1,4 +1,15 @@
 package com.example.koworkers.model;
 
+import android.graphics.Point;
+
+import com.example.koworkers.model.pieces.Piece;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+    HashMap<Piece,Point> hive= new HashMap(); //Hashmap med piece som key
+    public void movePiece(Piece piece, Point point){
+        hive.put(piece, point); //Tar en ny position och kopplar till piece å ändrar på så sätt piece-position.
+    }
 }
