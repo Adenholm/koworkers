04abb02a16a80d0267f0e755d730de38fcfa0503
Sara Sehdo <sarasehdo@gmail.com>
2021-09-21 23:56:53 +0200

    Made a superclass superPieceFragments
    PieceFragments extends the superclass, might have to redo later but is thought of having pieceStack extending the superclass too

--- app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
+++ app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
@@ -11,28 +11,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.koworkers.R;
+import com.example.koworkers.model.Colour;
+import com.example.koworkers.model.pieces.Piece;
+import com.example.koworkers.viewmodel.PieceViewModel;
 
-public class PieceFragment extends Fragment {
+public class PieceFragment extends superPieceFragment {
 
-    private PieceViewModel mViewModel;
 
-    public static PieceFragment newInstance() {
-        return new PieceFragment();
+    public PieceFragment(Piece piece) {
+        super(piece);
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.piece_fragment, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PieceViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
---
+++ app/src/main/java/com/example/koworkers/ui/piece/superPieceFragment.java
@@ -0,0 +1,72 @@
+package com.example.koworkers.ui.piece;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.koworkers.R;
+import com.example.koworkers.model.Colour;
+import com.example.koworkers.model.pieces.Piece;
+import com.example.koworkers.viewmodel.PieceViewModel;
+
+public class superPieceFragment extends Fragment {
+    private PieceViewModel mViewModel;
+
+    private ImageView hexagonImage;
+    private ImageView insectImage;
+    private final Piece piece;
+
+    public superPieceFragment(Piece piece){
+        this.piece = piece;
+    }
+
+
+   /* public static PieceFragment newInstance() {
+        return new PieceFragment();
+    }*/
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.piece_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PieceViewModel.class);
+
+        hexagonImage = getView().findViewById(R.id.hexagonImage);
+        insectImage = getView().findViewById(R.id.insectImage);
+
+        if(piece.getColour().equals(Colour.WHITE)){
+            hexagonImage.setImageResource(R.drawable.white_hexagon);
+        } else{
+            hexagonImage.setImageResource(R.drawable.black_hexagon);
+        }
+
+        /*if(piece.getInsect().equals(Insect.ant)){
+            insectImage.setImageResource(R.drawable.ant);
+        }
+        if(piece.getInsect().equals(Insect.spider)){
+            insectImage.setImageResource(R.drawable.spider);
+        }
+        if(piece.getInsect().equals(Insect.queen)){
+            insectImage.setImageResource(R.drawable.queen);
+        }
+        if(piece.getInsect().equals(Insect.beetle)){
+            insectImage.setImageResource(R.drawable.beetle);
+        }*/
+
+
+
+    }
+
+}
