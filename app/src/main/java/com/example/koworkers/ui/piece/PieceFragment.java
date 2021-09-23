9b2992b9be426724f2222cd32d3fe85a77f454b0
Sara Sehdo <sarasehdo@gmail.com>
2021-09-23 16:32:27 +0200

    Deleted superPieceFragment.java and moved the code back to PieceFragment.java

--- app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
+++ app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
@@ -18,11 +18,60 @@ import com.example.koworkers.model.Colour;
 import com.example.koworkers.model.pieces.Piece;
 import com.example.koworkers.viewmodel.PieceViewModel;
 
-public class PieceFragment extends superPieceFragment {
+public class PieceFragment extends Fragment {
+    private PieceViewModel mViewModel;
+
+    private ImageView hexagonImage;
+    private ImageView insectImage;
+    private final Piece piece;
+
+    public PieceFragment(Piece piece){
+        this.piece = piece;
+    }
 
 
-    public PieceFragment(Piece piece) {
-        super(piece);
+   /* public static PieceFragment newInstance() {
+        return new PieceFragment();
+    }*/
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.piece_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PieceViewModel.class);
+
+        hexagonImage = getView().findViewById(R.id.hexagonImage);
+        insectImage = getView().findViewById(R.id.insectImage);
+
+        if(piece.getColour().equals(Colour.WHITE)){                     //
+            hexagonImage.setImageResource(R.drawable.white_hexagon);
+        } else{
+            hexagonImage.setImageResource(R.drawable.black_hexagon);
+        }
+
+        /*if(piece.getInsect().equals(Insect.ant)){
+            insectImage.setImageResource(R.drawable.ant);
+        }
+        if(piece.getInsect().equals(Insect.spider)){
+            insectImage.setImageResource(R.drawable.spider);
+        }
+        if(piece.getInsect().equals(Insect.queen)){
+            insectImage.setImageResource(R.drawable.queen);
+        }
+        if(piece.getInsect().equals(Insect.beetle)){
+            insectImage.setImageResource(R.drawable.beetle);
+
+        if(piece.getInsect().equals(Insect.grasshopper)){
+            insectImage.setImageResource(R.drawable.grasshopper);
+        }*/
+
+
+
     }
 
 
