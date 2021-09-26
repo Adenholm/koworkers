d434d10df7bd7bdcf415f3d29e7b2a744db7299c
Adenholm <adenholm@student.chalmers.se>
2021-09-26 22:10:19 +0200

    created a populate and update method to the playerhand view and did some changes to the stackfragment class

--- app/src/main/java/com/example/koworkers/ui/playerhand/PlayerhandFragment.java
+++ app/src/main/java/com/example/koworkers/ui/playerhand/PlayerhandFragment.java
@@ -1,5 +1,6 @@
 package com.example.koworkers.ui.playerhand;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -14,12 +15,16 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.example.koworkers.R;
+import com.example.koworkers.model.pieces.IPiece;
+
+import java.util.ArrayList;
 
 public class PlayerhandFragment extends Fragment {
 
     private LinearLayout handLinearLayout;
 
     private PlayerhandViewModel mViewModel;
+
 
     public static PlayerhandFragment newInstance() {
         return new PlayerhandFragment();
@@ -41,8 +46,43 @@ public class PlayerhandFragment extends Fragment {
         populateHand();
     }
 
-    private void populateHand(){
+    public void update(){
+        populateHand();
+    }
 
+    /**
+     * creates stacks of pieces and adds to the linear layout
+     */
+    private void populateHand(){
+        ArrayList<PieceStackFragment> pieceStacks = new ArrayList<>();
+        boolean stackAlreadyExists = false;
+        for(IPiece piece: mViewModel.getPieces()){
+            for (PieceStackFragment piecestack : pieceStacks) {
+                if (piecestack.getPiece().getImageResource() == piece.getImageResource()) {
+                    piecestack.incNumberOfPieces();
+                    stackAlreadyExists = true;
+                    break;
+                }
+            }
+            if(!stackAlreadyExists){
+                pieceStacks.add(PieceStackFragment.newInstance(piece));
+            }
+            stackAlreadyExists = false;
+        }
+        addToLinearLayout(pieceStacks);
+    }
+
+    /**
+     * adds the provided list of PieceStackFragments to the Linear Layout
+     * @param pieceStacks to be added to Linear Layout
+     */
+    private void addToLinearLayout(ArrayList<PieceStackFragment> pieceStacks){
+        assert getFragmentManager() != null;
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        for(PieceStackFragment pieceStack: pieceStacks){
+            ft.add(R.id.handLinearLayout,pieceStack);
+        }
+        ft.commit();
     }
 
 }
