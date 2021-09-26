fbd3e1ce8e72cf3e9ec206c42d95d4ecd93268f6
Adenholm <adenholm@student.chalmers.se>
2021-09-26 18:46:10 +0200

    implemented the IPublisher InterFace;

--- app/src/main/java/com/example/koworkers/model/PlayerTurn.java
+++ app/src/main/java/com/example/koworkers/model/PlayerTurn.java
@@ -1,18 +1,40 @@
 package com.example.koworkers.model;
 
-public class PlayerTurn {
+import java.util.ArrayList;
+
+public class PlayerTurn implements IPublisher{
+
     private final Board board = new Board();
     private final PlayerHand blackHand = new PlayerHand();
     private final PlayerHand whiteHand = new PlayerHand();
+
     private PlayerHand currentPlayer;
 
     private int whiteNumberOfTurns;
     private int blackNumberOfTurns;
 
-    public PlayerTurn(){
+    private final ArrayList<ISubscriber> subscribers;
+
+    public PlayerTurn(ArrayList<ISubscriber> subscribers){
+        this.subscribers = subscribers;
         currentPlayer = whiteHand;
     }
 
+    @Override
+    public void subscribe(ISubscriber subscriber){
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    private void notifySubscribers(){
+        for(ISubscriber subscriber: subscribers){
+            subscriber.update;
+        }
+    }
+
+    /**
+     * Changes currentPlayer to the other colour that's not currently playing
+     */
     private void switchPlayer(){
         if (currentPlayer == whiteHand){
             currentPlayer = blackHand;
