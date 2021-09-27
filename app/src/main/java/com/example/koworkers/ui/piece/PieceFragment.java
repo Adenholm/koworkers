b004292b3a89390cfc7d5558a722612397bad6ec
sarasehdo <71220961+sarasehdo@users.noreply.github.com>
2021-09-27 22:59:15 +0200

    Adding images to piece

--- app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
+++ app/src/main/java/com/example/koworkers/ui/piece/PieceFragment.java
@@ -11,16 +11,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.koworkers.R;
+import com.example.koworkers.model.Colour;
+import com.example.koworkers.model.pieces.Ant;
+import com.example.koworkers.model.pieces.IPiece;
 
 public class PieceFragment extends Fragment {
 
     private PieceViewModel mViewModel;
 
-    public static PieceFragment newInstance() {
-        return new PieceFragment();
+    private ImageView hexagonImage;
+    private ImageView insectImage;
+
+    private final IPiece piece;
+
+    public PieceFragment(IPiece piece) {
+        this.piece = piece;
     }
+
+   /* public static PieceFragment newInstance() {
+        return new PieceFragment();
+    }*/
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -32,7 +45,21 @@ public class PieceFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(PieceViewModel.class);
+        hexagonImage = getView().findViewById(R.id.hexagonImage);
+        insectImage = getView().findViewById(R.id.insectImage);
+
+        if(piece.getColour().equals(Colour.WHITE)){
+            hexagonImage.setImageResource(R.drawable.white_hexagon);
+        }else{
+            hexagonImage.setImageResource(R.drawable.black_hexagon); //TODO ändra till svart hexagon
+        }
+
+        insectImage.setImageResource(piece.getImageResource());
         // TODO: Use the ViewModel
+    }
+
+    public void drawPiece(){
+
     }
 
 }
--- app/src/main/java/com/example/koworkers/ui/piece/PieceViewModel.java
+++ app/src/main/java/com/example/koworkers/ui/piece/PieceViewModel.java
@@ -2,6 +2,14 @@ package com.example.koworkers.ui.piece;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.koworkers.model.pieces.IPiece;
+
 public class PieceViewModel extends ViewModel {
+
+    private IPiece piece;
+
+
+
+
     // TODO: Implement the ViewModel
 }
