d8eb3960fa1b02e77fedc6ad8458b2f4e1d12775
sarasehdo <71220961+sarasehdo@users.noreply.github.com>
2021-09-27 23:00:51 +0200

    Testing mainActivity with (piece)fragment

--- app/src/main/java/com/example/koworkers/MainActivity.java
+++ app/src/main/java/com/example/koworkers/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.koworkers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.koworkers.model.PlayerHand;
+import com.example.koworkers.ui.playerhand.PlayerhandFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;      //playerhandlistan
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       /* if (savedInstanceState == null){
+
+
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.playerHand, PlayerhandFragment.class, null)
+                    .commit();
+        }*/
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.piece_fragment);
+
+
+
+
+        //linearLayout=findViewById(R.id.handLinearLayout);
     }
 }
