fe4aa0bc039b1baf5f99e39c3e329a27fb536785
stihan01 <stihan01@gmail.com>
2021-09-28 10:04:24 +0200

    added test for ant.getPossibleMoves

--- app/src/test/java/com/example/koworkers/ExampleUnitTest.java
+++ app/src/test/java/com/example/koworkers/ExampleUnitTest.java
@@ -26,12 +26,14 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
+
+
     @Test
     public void antMoves_isCorrect(){
         Piece ant = new Ant(Colour.BLACK);
         ArrayList<Point> board = new ArrayList<>(Arrays.asList(new Point(0,0),new Point(0,1),new Point(0,2)));
         Point[] pointArray = new Point[] {new Point(-1,1),new Point(0,2),new Point(1,1),new Point(0,3),new Point(1,3),new Point(2,2)};
-        System.out.print(ant.getPossibleMoves(board).toArray());
+        System.out.print(ant.getPossibleMoves(board).toString());
         assertArrayEquals(pointArray,ant.getPossibleMoves(board).toArray());
     }
 }
