304e58eccc2d48e523eef18493f2c817f3a7698c
Adenholm <adenholm@student.chalmers.se>
2021-10-02 18:24:41 +0200

    subscribed the views to hive class (needs to be changed later)

--- app/src/main/java/com/example/koworkers/MainActivity.java
+++ app/src/main/java/com/example/koworkers/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.koworkers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
+
+import com.example.koworkers.model.Hive;
+import com.example.koworkers.model.Isubscriber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        FragmentContainerView boardFragmentContainerView = findViewById(R.id.boardFragmentContainerView);
+        Isubscriber boardFragment = boardFragmentContainerView.getFragment();
+        Hive.getInstance().subscribe(boardFragment);
+
+        FragmentContainerView playerHandFragmentContainerView = findViewById(R.id.playerHandFragmentContainerView);
+        Isubscriber playerHandFragment = playerHandFragmentContainerView.getFragment();
+        Hive.getInstance().subscribe(playerHandFragment);
     }
 }
