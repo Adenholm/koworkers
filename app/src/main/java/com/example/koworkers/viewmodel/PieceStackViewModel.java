33bf3c4859b2fa87293e227e28fc8014c74b75e1
Adenholm <adenholm@student.chalmers.se>
2021-10-07 11:22:27 +0200

    removed unnecessary classes

--- app/src/main/java/com/example/koworkers/viewmodel/PieceStackViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.koworkers.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class PieceStackViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/koworkers/viewmodel/PieceViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.koworkers.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class PieceViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
