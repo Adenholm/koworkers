ed232dcffcb367c9aa1e5965666cd90555bc26f6
Adenholm <adenholm@student.chalmers.se>
2021-10-12 16:36:46 +0200

    switched from fragmentconatainerview to framelayout and added the fragments manually in order to get rid of the signleton pattern in hive

--- app/src/main/java/com/example/koworkers/MainActivity.java
+++ app/src/main/java/com/example/koworkers/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.koworkers;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.koworkers.model.Hive;
 import com.example.koworkers.model.Isubscriber;
+import com.example.koworkers.view.BoardFragment;
+import com.example.koworkers.view.PlayerhandFragment;
 import com.example.koworkers.view.WinScreenFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Hive hive = new Hive();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
 
-        FragmentContainerView boardFragmentContainerView = findViewById(R.id.boardFragmentContainerView);
-        Isubscriber boardFragment = boardFragmentContainerView.getFragment();
-        Hive.getInstance().subscribe(boardFragment);
+        BoardFragment boardFragment = new BoardFragment();
+        FrameLayout boardFrame = findViewById(R.id.boardFrame);
+        hive.subscribe(boardFragment);
 
-        FragmentContainerView playerHandFragmentContainerView = findViewById(R.id.playerHandFragmentContainerView);
-        Isubscriber playerHandFragment = playerHandFragmentContainerView.getFragment();
-        Hive.getInstance().subscribe(playerHandFragment);
+        PlayerhandFragment playerhandFragment = new PlayerhandFragment();
+        FrameLayout playerhandFrame = findViewById(R.id.playerhandFrame);
+        hive.subscribe(playerhandFragment);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.boardFrame, boardFragment);
+        ft.add(R.id.playerhandFrame, playerhandFragment);
+        ft.commit();
+
     }
 
     public void showWinPopup(String winner){
