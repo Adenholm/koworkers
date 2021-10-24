3f71987dd57734c167a0c70eafd72dda23f95a4b
Adenholm <adenholm@student.chalmers.se>
2021-10-24 23:00:31 +0200

    added documents and comments

--- app/src/main/java/com/example/koworkers/model/IWinSubscriber.java
+++ app/src/main/java/com/example/koworkers/model/IWinSubscriber.java
@@ -1,5 +1,15 @@
 package com.example.koworkers.model;
 
+/**
+ * An interface for classes that wants to get notified if a player wins
+ *
+ * @author Hanna Adenholm
+ */
 public interface IWinSubscriber {
+    /**
+     * Runs if a player wins.
+     *
+     * @param winningColour the colour of the winning player.
+     */
     void playerWon(Colour winningColour);
 }
